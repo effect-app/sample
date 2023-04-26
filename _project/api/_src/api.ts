jfet97/boilerplate

Extract BlogPost items from a global, into a BlogPostRepository service.

apps/api-api/_src/api.ts
@@ -3,7 +3,7 @@ import * as Ex from "@effect-ts-app/infra/express/index"
 import type { ApiMainConfig } from "./config.js"
 import * as MW from "./middleware/index.js"
 import * as R from "./routes.js"
-import { Operations, StoreMaker, UserRepository } from "./services.js"
+import { BlogPostLive, Operations, StoreMaker, UserRepository } from "./services.js"
 import { Events } from "./services/Events.js"
 
 const routes = Effect.struct(R)
@@ -32,6 +32,7 @@ export function api(cfg: ApiMainConfig) {
     > logServerStart
 
   const services = Events.Live
+    > BlogPostLive
     > StoreMaker.Live(Config(cfg.storage))
     > UserRepository.Live(
       cfg.fakeUsers === "sample" ? "sample" : ""

effect-app/sample

_project/api/_src/api.ts
@@ -3,7 +3,7 @@ import { writeOpenapiDocs } from "@effect-app/infra/api/writeDocs"
 import type { ApiMainConfig } from "./config.js"
 import * as MW from "./middleware/index.js"
 import * as R from "./routes.js"
-import { Operations, StoreMaker, UserRepo } from "./services.js"
+import { BlogPostRepoLive, Operations, StoreMaker, UserRepo } from "./services.js"
 import { Events } from "./services/Events.js"
 
 const routes = Effect.all(R)
@@ -28,6 +28,7 @@ export function api(cfg: ApiMainConfig) {
     > logServerStart
 
   const services = Events.Live
+    > BlogPostRepoLive
     > StoreMaker.Live(Config(cfg.storage))
     > UserRepo.Live(
       cfg.fakeUsers === "sample" ? "sample" : ""
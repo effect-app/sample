43c7917326bf5ce32a696c99fe85d9ce78eee644
Patrick Roza <contact@patrickroza.com>
2023-01-18 12:27:28 +0100

    Extract BlogPost items from a global, into a BlogPostRepository service.

--- _project/api/_src/Usecases/Blog.Controllers.ts
+++ _project/api/_src/Usecases/Blog.Controllers.ts
@@ -1,15 +1,19 @@
 import { BlogPost } from "@effect-app-boilerplate/models/Blog"
 import { BlogRsc } from "@effect-app-boilerplate/resources"
-
-const items: BlogPost[] = []
+import { BlogPostRepo } from "api/services.js"
 
 const blog = matchFor(BlogRsc)
+const GetPosts = blog.matchGetPosts(
+  { BlogPostRepo },
+  (_, { blogPostRepo }) => blogPostRepo.all.map((items) => ({ items }))
+)
 
-const GetPosts = blog.matchGetPosts({}, () => Effect({ items }))
-
-const CreatePost = blog.matchCreatePost({}, (req) =>
-  Effect(new BlogPost({ ...req }))
-    .tap((post) => Effect(items.push(post)))
-    .map((_) => _.id))
+const CreatePost = blog.matchCreatePost(
+  { BlogPostRepo },
+  (req, { blogPostRepo }) =>
+    Effect(new BlogPost({ ...req }))
+      .tap(blogPostRepo.save)
+      .map((_) => _.id)
+)
 
 export default blog.controllers({ GetPosts, CreatePost })
