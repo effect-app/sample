4d31a9d52c3058e0d3b02371b0afaeb4cb2b3668
Patrick Roza <contact@patrickroza.com>
2025-01-18 21:32:13 +0100

    fix helloworld controllers

--- api/src/controllers.ts
+++ api/src/controllers.ts
@@ -1,7 +1,8 @@
 // codegen:start {preset: barrel, include: ./*/controllers.ts, import: default}
 import accountsControllers from "./Accounts/controllers.js"
 import blogControllers from "./Blog/controllers.js"
+import helloWorldControllers from "./HelloWorld/controllers.js"
 import operationsControllers from "./Operations/controllers.js"
 
-export { accountsControllers, blogControllers, operationsControllers }
+export { accountsControllers, blogControllers, helloWorldControllers, operationsControllers }
 // codegen:end
